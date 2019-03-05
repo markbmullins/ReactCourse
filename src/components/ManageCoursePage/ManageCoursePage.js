477eb69b73723c26bc7f51086c1bb801f7b8cec8
markbmullins <markmullins95@gmail.com>
2019-03-05 14:56:15 -0500

    fixed submit errors

--- src/components/ManageCoursePage/ManageCoursePage.js
+++ src/components/ManageCoursePage/ManageCoursePage.js
@@ -6,6 +6,7 @@ import { saveCourse } from "../../api/courseApi";
 class ManageCoursePage extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       course: {
         id: null,
@@ -22,9 +23,9 @@ class ManageCoursePage extends Component {
     history: PropTypes.object.isRequired
   };
 
-  handleFormChange = event => {
+  handleChange = event => {
     const course = { ...this.state.course };
-    //computed property syntxa
+    // computed property syntax
     course[event.target.name] = event.target.value;
     this.setState({ course });
   };
@@ -32,15 +33,17 @@ class ManageCoursePage extends Component {
   handleSubmit = event => {
     event.preventDefault();
     const errors = {};
-    if (!this.state.course.title) errors.title = "Title is required";
-    if (!this.state.course.category) errors.category = "Category is required";
-    if (!this.state.course.authorId) errors.authorId = "Author is required";
 
-    if (Object.keys(errors)) {
+    if (!this.state.course.title) errors.title = "Title is required.";
+    if (!this.state.course.category) errors.category = "Category is required.";
+    if (!this.state.course.authorId) errors.authorId = "Author is required.";
+
+    // Is there at least one property on the errors object? If so, validation failed.
+    if (Object.keys(errors).length > 0) {
       this.setState({ errors });
       return;
     }
-    debugger;
+
     saveCourse(this.state.course).then(() => {
       this.props.history.push("/courses");
     });
@@ -49,33 +52,35 @@ class ManageCoursePage extends Component {
   render() {
     return (
       <div>
-        <h1>ManageCourse</h1>
-
+        <h1>Manage Course</h1>
         <form onSubmit={this.handleSubmit}>
           <TextInput
             label="Title"
             id="title"
             name="title"
-            onChange={this.handleFormChange}
+            onChange={this.handleChange}
             value={this.state.course.title}
             error={this.state.errors.title}
           />
+
           <TextInput
             label="Category"
             id="category"
             name="category"
-            onChange={this.handleFormChange}
+            onChange={this.handleChange}
             value={this.state.course.category}
             error={this.state.errors.category}
           />
+
           <TextInput
             label="Author"
             id="author"
             name="authorId"
-            onChange={this.handleFormChange}
+            onChange={this.handleChange}
             value={this.state.course.authorId}
             error={this.state.errors.authorId}
           />
+
           <button type="submit">Save Course</button>
         </form>
       </div>
