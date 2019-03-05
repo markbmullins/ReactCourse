7ea4f931bf97ef0718a5da537cafaaf5fc189b43
markbmullins <markmullins95@gmail.com>
2019-03-05 13:56:09 -0500

    updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../HomePage";
 import CoursesPage from "../CoursesPage";
 import Nav from "../Nav";
 import ManageCoursePage from "../ManageCoursePage";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 const App = () => {
   return (
     <div>
       <Nav />
-      <Route path="/" component={HomePage} exact />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/course" component={ManageCoursePage} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course" component={ManageCoursePage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 };
