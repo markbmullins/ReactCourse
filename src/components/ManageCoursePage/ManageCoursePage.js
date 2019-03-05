bd6e48b4741dd4936521901224dafcddb71cbf71
markbmullins <markmullins95@gmail.com>
2019-03-05 13:56:25 -0500

    created reusable form component

--- src/components/ManageCoursePage/ManageCoursePage.js
+++ src/components/ManageCoursePage/ManageCoursePage.js
@@ -1,9 +1,51 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import TextInput from "../TextInput";
 
 class ManageCoursePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        id: null,
+        title: "",
+        category: "",
+        slug: null,
+        authorId: null
+      }
+    };
+  }
+
+  handleFormChange = event => {
+    const course = { ...this.state.course };
+    //computed property syntxa
+    course[event.target.name] = event.target.value;
+    this.setState({ course });
+  };
+
   render() {
-    return <h1>ManageCourse</h1>;
+    return (
+      <div>
+        <h1>ManageCourse</h1>
+
+        <form>
+          <TextInput
+            label="Title"
+            id="title"
+            name="title"
+            onChange={this.handleFormChange}
+            value={this.state.course.title}
+          />
+          <TextInput
+            label="Category"
+            id="category"
+            name="category"
+            onChange={this.handleFormChange}
+            value={this.state.course.category}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
