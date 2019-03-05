937ee83e84cd69b8b0e8a2d0ff54ab3cc5879abd
markbmullins <markmullins95@gmail.com>
2019-03-05 11:28:16 -0500

    Wrapped entry point

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React from "react";
-import { getCourses, deleteCourse } from "./api/courseApi";
-import CourseTable from "./CourseTable";
+import { Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import CoursesPage from "./CoursesPage";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      courses: []
-    };
-    //bind in constructor
-    //this.handleDelete = this.handleDelete.bind(this);
-  }
-  componentDidMount() {
-    getCourses().then(courses => {
-      this.setState({ courses });
-    });
-  }
-  handleDelete = courseId => {
-    deleteCourse(courseId).then(() => {
-      const courses = this.state.courses.filter(
-        course => course.id !== courseId
-      );
-      this.setState({ courses });
-    });
-  };
-  render() {
-    return (
-      <>
-        <h1>App</h1>
-        <h2>Courses</h2>
-        {this.state.courses.length === 0 ? (
-          <p>No courses to display.</p>
-        ) : (
-          <CourseTable
-            courses={this.state.courses}
-            onClickDelete={this.handleDelete}
-          />
-        )}
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/courses" component={CoursesPage} />
+    </div>
+  );
+};
 
 export default App;
