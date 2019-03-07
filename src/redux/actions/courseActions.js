86099cd9299da65f3a2b3456039b497091d54dd5
markbmullins <markmullins95@gmail.com>
2019-03-07 11:05:25 -0500

    added actions and reducers for create and update

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -4,10 +4,17 @@ import * as courseApi from "../../api/courseApi";
 // Action creator
 function loadCoursesSuccess(courses) {
   // Action
-  return {
-    type: types.LOAD_COURSES_SUCCESS,
-    courses
-  };
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+function updateCourseSucess(course) {
+  // Action
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+function createCourseSuccess(course) {
+  // Action
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 //Thunk:
@@ -16,6 +23,17 @@ export function loadCourses() {
     return courseApi.getCourses().then(courses => {
       //   debugger;
       dispatch(loadCoursesSuccess(courses));
+    });
+  };
+}
+
+//Thunk:
+export function saveCourse(course) {
+  return function(dispatch) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id
+        ? dispatch(updateCourseSucess(savedCourse))
+        : dispatch(createCourseSuccess(savedCourse));
     });
   };
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -5,6 +5,12 @@ export default function courseReducer(state = [], action) {
     case types.LOAD_COURSES_SUCCESS:
       //   debugger;
       return [...action.courses];
+    case types.CREATE_COURSE_SUCCESS:
+      return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     default:
       return state;
   }
